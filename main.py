eea4870c5861fa2b42fafee5f51f7e55202ecb3b
Jacob Edwards <jacob.john1@talktalk.net>
2022-08-30 22:30:22 +0100

    initial commit

--- main.py
+++ main.py
@@ -27,15 +27,51 @@ PAYMENT_TOKEN = ''
 
 
 async def start(update: Update, context: CallbackContext) -> None:
-    pass
+    userName = update.effective_user.first_name
+    userID = update.effective_user.id
+    userKey = f'video2mp3:{userID}'
+    numUses = r.scard(userKey)
+
+    if numUses == 0:
+        await update.message.reply_text(f'Hello {userName}\n\nWelcome to Youtube Video to MP3 Bot!\n\nThis bot is'
+                                        f' used convert a Youtube video _(ie a music video)_ to an MP3 file!\n\n'
+                                        f'To begin, simply send a Youtube video link, and the transcript will be '
+                                        f'sent to you.\n\n', parse_mode=Mark)
+
+    if not r.sismember('premium', userID):
+        await update.message.reply_text(f'You have {8 - numUses} uses remaining on your free trial.\n\nOr upgrade to '
+                                        f'Premium for unlimited use across a number of different bots!')
+        keyboard = [
+            [KeyboardButton("YouTube Video to MP3", callback_data="1")],
+            [
+                KeyboardButton("Premium", callback_data="2"),
+                KeyboardButton("Support!", callback_data="3"),
+            ],
+        ]
+    else:
+        await update.message.reply_text('Your account is premium!\n\nUnlimited use!')
+        keyboard = [
+            [KeyboardButton("YouTube Video to MP3", callback_data="1")],
+            [KeyboardButton("Support!", callback_data="3")],
+        ]
+
+    menu_markup = ReplyKeyboardMarkup(keyboard)
+    await update.message.reply_text('Send a URL to get started, or select an option below:', reply_markup=menu_markup)
 
 
 async def helpInfo(update: Update, context: CallbackContext) -> None:
-    pass
+    await update.message.reply_text('Help')
+    # to expand
 
 
+async def sendURL(update: Update, context: CallbackContext) -> None:
+    await update.message.reply_text('Send a Youtube Video URL to convert to an MP3 file:')
+
+
+# unknown command function
 async def unknownCommand(update: Update, context: CallbackContext) -> None:
-    pass
+    await update.message.reply_text('Unknown command\n\nPlease use /help for help, or send a YouTube Video URL to extra'
+                                    'ct the subtitle transcript!')
 
 
 async def checkURL(update: Update, context: CallbackContext, url) -> bool:
@@ -48,7 +84,21 @@ async def checkURL(update: Update, context: CallbackContext, url) -> bool:
 
 async def getMP3(update: Update, context: CallbackContext) -> None:
     userID = update.effective_user.id
-    userKey = f''
+    userKey = f'video2mp3:{userID}'
+    numUses = r.scard(userKey)
+
+    if numUses > 7 and not r.sismember('premium', userID):
+        # send message to user
+        return
+
+    url = update.effective_message.text
+    if not checkURL(update, context, url):
+        # inform user not video link
+        return
+
+
+async def button(update: Update, context: CallbackContext) -> None:
+
 
 
 # sends invoice to upgrade user to premium
@@ -56,7 +106,7 @@ async def upgrade(update: Update, context: CallbackContext) -> None:
     # checks that the user is premium or not
     if r.sismember('premium', update.effective_user.id):
         keyboard = [
-            [KeyboardButton("Get youtube video transcript!", callback_data="1")],
+            [KeyboardButton("YouTube Video to MP3", callback_data="1")],
             [KeyboardButton("Support!", callback_data="3")],
         ]
 
@@ -95,7 +145,7 @@ async def upgradeSuccessful(update: Update, context: CallbackContext) -> None:
     r.sadd('premium', update.effective_user.id)
 
     keyboard = [
-        [KeyboardButton("Extract subtitles!", callback_data="1")],
+        [KeyboardButton("YouTube Video to MP3", callback_data="1")],
         [KeyboardButton("Support!", callback_data="3")],
     ]
     menu_markup = ReplyKeyboardMarkup(keyboard)
@@ -104,7 +154,7 @@ async def upgradeSuccessful(update: Update, context: CallbackContext) -> None:
 
 
 def main() -> None:
-    application = Application.builder().token("5561745160:AAHLaEHPUZ1QGfdxcUrxnmJUKiI4WDo8pFY").build()
+    application = Application.builder().token("").build()
 
     # basic command handlers
     application.add_handler(CommandHandler('start', start))
@@ -115,7 +165,7 @@ def main() -> None:
 
     # handles the pre-made keyboard
     application.add_handler(MessageHandler(filters.Regex('Support!'), helpInfo))
-    application.add_handler(MessageHandler(filters.Regex('Extract subtitles!'), sendURL))
+    application.add_handler(MessageHandler(filters.Regex('YouTube Video to MP3'), sendURL))
     application.add_handler(MessageHandler(filters.Regex('Premium'), upgrade))
 
     # Pre-checkout handler to final check
